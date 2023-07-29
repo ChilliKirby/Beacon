db0e861939221f54d4d2a79ccdaa3d98d7d488ec
jesse <u1110728@utah.edu>
2023-07-28 23:03:37 -0600

    --Set up google maps service with development/debug build. --created .env file

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { styles } from './Styles.js';
+import { View, Text } from 'react-native';
+import  MapView  from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+import { useState } from 'react';
+import { PROVIDER_GOOGLE } from 'react-native-maps';
+
 
 export default function App() {
+
+ 
+
+    const [region, setRegion] = useState({
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+
+  const onRegionChange = (region) => {
+    this.setState({ region });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <MapView
+        provider= { PROVIDER_GOOGLE }
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        style={styles.mapContainer}
+        region={ region }
+        // onRegionChange={setRegion}
+      >
+        <Marker coordinate={region}/>
+      </MapView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
