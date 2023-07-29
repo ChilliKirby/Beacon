9c7457be5503e3cd3577ba0df3a0677e2c08d632
jesse <u1110728@utah.edu>
2023-07-29 00:22:40 -0600

    --map seems to work but showing wrong coordinates perhaps due to debug mode.

--- App.js
+++ App.js
@@ -1,43 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
 import { styles } from './Styles.js';
-import { View, Text } from 'react-native';
-import  MapView  from 'react-native-maps';
+import { Platform, View, Text } from 'react-native';
+import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { PROVIDER_GOOGLE } from 'react-native-maps';
+import * as Location from 'expo-location';
+import React from 'react';
 
 //jj
 export default function App() {
 
- 
 
-    const [region, setRegion] = useState({
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    });
+
+  const [region, setRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
 
   const onRegionChange = (region) => {
     this.setState({ region });
   }
 
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if(status !== 'granted'){
+        setErrorMsg("permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setRegion( setLocation(prevLoc => ({
+          ...prevLoc,
+          latitude: location.latitude,
+          longitude: location.longitude,
+        })));
+    })();
+  }, []);
+
+  let text = "waiting...";
+  
+  if(errorMsg){
+    text = errorMsg;
+  } else if(location){
+    text = JSON.stringify(location);
+    //const jsonObj = JSON.parse
+    // setLocation(prevLoc => ({
+    //   ...prevLoc,
+    //   latitude: location.latitude,
+    //   longitude: location.longitude,
+    // }))
+  }
+
   return (
     <View style={styles.container}>
-      
+      <Text>{text}</Text>
       <MapView
-        provider= { PROVIDER_GOOGLE }
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={region}
         style={styles.mapContainer}
-        region={ region }
-        // onRegionChange={setRegion}
+        region={region}
+      // onRegionChange={setRegion}
       >
-        <Marker coordinate={region}/>
+        <Marker coordinate={region} />
       </MapView>
     </View>
   );
