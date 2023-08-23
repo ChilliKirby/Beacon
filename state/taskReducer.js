83ef6b403acd789078bcdfc028b3d257f14fef21
jesse <u1110728@utah.edu>
2023-08-23 17:59:36 -0600

    a

---
+++ state/taskReducer.js
@@ -0,0 +1,23 @@
+
+const initialState = {
+    user: null,
+    token: null,
+    friends: [],
+    tasks: [],
+};
+
+const taskReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_USER':
+            return {
+                ...state,
+                user: action.payload,
+            };
+        
+        default:
+            return state;
+
+    }
+};
+
+export default rootReducer;
