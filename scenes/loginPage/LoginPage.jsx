d2a89b1d938856b6eb17f302dc10ab3954c73613
jesse <u1110728@utah.edu>
2023-08-25 20:59:15 -0600

    --Continued writing login code in LoginPage. Communication can now be made between server and client.

--- scenes/loginPage/LoginPage.jsx
+++ scenes/loginPage/LoginPage.jsx
@@ -11,19 +11,19 @@ import {
     statusCodes,
 } from '@react-native-google-signin/google-signin';
 import { sign } from "core-js/core/number"
-;
+    ;
 import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "../../state/index.js";
 
 
 
 
-const LoginPage = ({navigation}) => {
+const LoginPage = ({ navigation }) => {
 
-    const  userName = useSelector((state) => state.userName);
+    const userName = useSelector((state) => state.userName);
     const dispatch = useDispatch();
 
-   
+
     GoogleSignin.configure({
         webClientId: process.env.EXPO_PUBLIC_REACT_APP_WEB_CLIENT_ID,
         offlineAccess: true,
@@ -41,7 +41,7 @@ const LoginPage = ({navigation}) => {
         try {
             await GoogleSignin.hasPlayServices();
             const userInfo = await GoogleSignin.signIn();
-           setUser(userInfo);
+            setUser(userInfo);
             // //set email to be sent to Beacon server
             // setEmail(userInfo.user.email);
 
@@ -67,7 +67,9 @@ const LoginPage = ({navigation}) => {
 
     const getUser = async () => {
 
-        const userInfo =  await GoogleSignin.getCurrentUser();
+        
+        try{
+        const userInfo = await GoogleSignin.getCurrentUser();
         //  if(userInfo ){
         //     dispatch(setUser({
         //         userName: userInfo.user.email,
@@ -75,46 +77,59 @@ const LoginPage = ({navigation}) => {
         //      console.log(userName);
         // }
 
-         //set email to be sent to Beacon server
+        //set email to be sent to Beacon server
         //  setEmail(userInfo.user.email);
 
         //  //get id token from Google for user
         //  const tokens = await GoogleSignin.getTokens();
         //  setidToken(tokens.idToken);
         //  setResponse(true);
-        
 
         console.log("test");
         console.log(userInfo.user.email);
-            console.log(userInfo.idToken);
-            console.log(process.env.EXPO_PUBLIC_REACT_APP_WEB_CLIENT_ID);
+        console.log(userInfo.idToken);
+        console.log(process.env.EXPO_PUBLIC_REACT_APP_WEB_CLIENT_ID);
+
+        const loggedInResponse = await fetch("http://192.168.86.121:3001/auth/login",{
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                email: userInfo.user.email,
+                idToken: userInfo.idToken
+            })
+        })
+
+        
+        } catch(error){
+            console.log("error:", error);
+        }
     };
 
     const signInBeacon = async () => {
-        try{
+        try {
 
-        } catch (error){
+        } catch (error) {
 
         }
     };
 
     const signOut = async () => {
         try {
-          await GoogleSignin.signOut();
-          console.log("signed out");
-          //setState({ user: null }); // Remember to remove the user from your app's state as well
+            await GoogleSignin.signOut();
+            console.log("signed out");
+            //setState({ user: null }); // Remember to remove the user from your app's state as well
         } catch (error) {
-          console.error(error);
+            console.error(error);
         }
-      };
+    };
 
     const getTokens = async () => {
 
     };
 
-    const isSignedIn = async() => {
+    const isSignedIn = async () => {
         const isIn = await GoogleSignin.isSignedIn();
-        if (isIn){
+        if (isIn) {
             getUser();
         }
     }
@@ -133,10 +148,10 @@ const LoginPage = ({navigation}) => {
                 <GoogleSigninButton
                     size={GoogleSigninButton.Size.Wide}
                     color={GoogleSigninButton.Color.Dark}
-                    onPress={ signInGoogle }
+                    onPress={signInGoogle}
                 />
-                <Button title={"temp"} onPress={ getUser} >butt</Button>
-                <Button title={"out"} onPress={ signOut} >out</Button>
+                <Button title={"temp"} onPress={getUser} >butt</Button>
+                <Button title={"out"} onPress={signOut} >out</Button>
                 <Text style={styles.textInfo}>{userName}</Text>
             </SafeAreaView>
         </View>
